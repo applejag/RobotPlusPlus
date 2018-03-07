c4d71fc9231fd088b7fb685245d42dc97935f505
Kalle Jillheden <kalle@jillheden.se>
2018-03-07 17:24:56 +0100

    Attribute utility

---
+++ RobotPlusPlus.Compiler/Utility/AttributesUtilities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace RobotPlusPlus.Utility
+{
+	public static class AttributesUtilities
+	{
+		public static TAttr GetEnumAttribute<TAttr>(this Enum enumVal) where TAttr : Attribute
+		{
+			return enumVal.GetEnumAttributes<TAttr>().FirstOrDefault();
+		}
+
+		public static TAttr[] GetEnumAttributes<TAttr>(this Enum enumVal) where TAttr : Attribute
+		{
+			MemberInfo[] memInfo = enumVal.GetType().GetMember(enumVal.ToString());
+			object[] attributes = memInfo[0].GetCustomAttributes(typeof(TAttr), true);
+			return (TAttr[])attributes;
+		}
+
+		public static TAttr GetPropertyAttribute<TClass, TAttr>(string propertyName)
+		{
+			return (TAttr)typeof(TClass).GetProperty(propertyName).GetCustomAttributes(typeof(TAttr), true)
+				.FirstOrDefault();
+		}
+
+		public static TAttr[] GetPropertyAttributes<TClass, TAttr>(string propertyName)
+		{
+			return typeof(TClass).GetProperty(propertyName).GetCustomAttributes(typeof(TAttr), true)
+				.Cast<TAttr>().ToArray();
+		}
+
+		public static class Presets
+		{
+			public static int? GetMinLength<TClass>(string propertyName)
+			{
+				return GetPropertyAttribute<TClass, MinLengthAttribute>(propertyName)?.Length;
+			}
+
+			public static int? GetMaxLength<TClass>(string propertyName)
+			{
+				return GetPropertyAttribute<TClass, MaxLengthAttribute>(propertyName)?.Length;
+			}
+
+			public static string GetDisplayName<TClass>(string propertyName)
+			{
+				return GetPropertyAttribute<TClass, DisplayAttribute>(propertyName)?.Name;
+			}
+		}
+	}
+}
