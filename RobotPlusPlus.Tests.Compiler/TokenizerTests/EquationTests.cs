90d5b7837ece200686d324d134ce32525e14e96d
Kalle Jillheden <kalle@jillheden.se>
2018-03-09 13:23:44 +0100

    Added sample math test. Just as an extra check that it works. And it did. :tada:

---
+++ RobotPlusPlus.Tests.Compiler/TokenizerTests/EquationTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobotPlusPlus.Tokenizing;
+
+namespace RobotPlusPlus.Tests.TokenizerTests
+{
+	[TestClass]
+	public class EquationTests
+	{
+		[TestMethod]
+		public void Tokenize_SimpleEquation()
+		{
+			// Arrange
+			const string input = "y=x*x+(50-z)*.5";
+
+			// Act
+			Token[] result = Tokenizer.Tokenize(input);
+
+			// Assert
+			Utility.AssertTokenTypes(result,
+				TokenType.Identifier,	// y
+				TokenType.Operator,     // =
+				TokenType.Identifier,   // x
+				TokenType.Operator,     // *
+				TokenType.Identifier,   // x
+				TokenType.Operator,     // +
+				TokenType.Punctuators,  // (
+				TokenType.Literal,      // 50
+				TokenType.Operator,     // -
+				TokenType.Identifier,   // z
+				TokenType.Punctuators,  // )
+				TokenType.Operator,     // *
+				TokenType.Literal       // .5
+				);
+		}
+	}
+}
