a5049099c845bc3cdd372a34359578d812d69522
kalle (jag) <kalle.jillheden@me.com>
2018-03-14 23:27:04 +0100

    Added CommandLineUtils to console Also added initial commandline options with some destination folder parsing

--- RobotPlusPlus.Console/Program.cs
+++ RobotPlusPlus.Console/Program.cs
@@ -1,15 +1,14 @@
 ﻿
+using McMaster.Extensions.CommandLineUtils;
+
 namespace RobotPlusPlus.Console
 {
-	using System;
-
 	public class Program
 	{
+
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			Console.WriteLine(string.Join("\n", args));
-			Console.ReadKey(true);
+			CommandLineApplication.Execute<ProgramOptions>(args);
 		}
 	}
 }
---
+++ RobotPlusPlus.Console/ProgramOptions.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace RobotPlusPlus.Console
+{
+	using System;
+
+	[HelpOption]
+	public class ProgramOptions
+	{
+		[Required]
+		[FileExists]
+		[Argument(0, Name = "Script",
+			Description = "Required. The `.robotpp` script to be parsed.")]
+		public string Script { get; }
+
+		[Option(ShortName = "d", LongName = "Destination", ValueName = "Folder",
+			Description = "The destination for the compiled `.robot` script. " +
+			              "If none is supplied, it will be same folder and filename " +
+			              "as the `.robotpp` script (with `.robot` extension).")]
+		public string Destination { get; }
+
+		private void OnExecute()
+		{
+			string dest = Path.Combine(Path.GetDirectoryName(Script), Destination ?? "", Path.ChangeExtension(Path.GetFileName(Script), ".robot"));
+
+			Console.WriteLine($"I should use the file \"{Script}\" apprently, and output to \"{dest}\".");
+			Console.ReadLine();
+		}
+	}
+}
