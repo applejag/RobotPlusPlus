b3d7937d40250e76b48a45b2d683ee0846179c92
kalle (jag) <kalle.jillheden@me.com>
2018-03-19 22:49:44 +0100

    Added parentases tests

---
+++ RobotPlusPlus.Core.Tests/CompilerTests/SimpleParentasesTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobotPlusPlus.Core.Compiling;
+
+namespace RobotPlusPlus.Core.Tests.CompilerTests
+{
+	[TestClass]
+	public class SimpleParentasesTests
+	{
+
+		[TestMethod]
+		public void Compile_ParentasesAroundLiteral()
+		{
+			// Arrange
+			string output = Compiler.Compile("x=(1)");
+
+			// Assert
+			Assert.AreEqual("♥x=1", output);
+		}
+
+		[TestMethod]
+		public void Compile_DoubleParentasesAroundLiteral()
+		{
+			// Arrange
+			string output = Compiler.Compile("x=((1))");
+
+			// Assert
+			Assert.AreEqual("♥x=1", output);
+		}
+
+		[TestMethod]
+		public void Compile_ParentasesInIfStatement()
+		{
+			// Arrange
+			string output = Compiler.Compile("if (x > 0)");
+			const string expected = "jump ➜noif if ⊂!(x>0)⊃\n" +
+			                        "➜noif";
+
+			// Assert
+			Assert.AreEqual(expected, output);
+		}
+
+		[TestMethod]
+		public void Compile_DoubleParentasesInIfStatement()
+		{
+			// Arrange
+			string output = Compiler.Compile("if ((x > 0))");
+			const string expected = "jump ➜noif if ⊂!(x>0)⊃\n" +
+			                        "➜noif";
+
+			// Assert
+			Assert.AreEqual(expected, output);
+		}
+
+		[TestMethod]
+		public void Compile_ParentasesPriorityNotNeeded()
+		{
+			// Arrange
+			string output = Compiler.Compile("x=(1*5)+5");
+
+			// Assert
+			Assert.AreEqual("♥x=1*5+5", output);
+		}
+
+		[TestMethod]
+		public void Compile_ParentasesPriorityNeeded()
+		{
+			// Arrange
+			string output = Compiler.Compile("x=1*(5+5)");
+
+			// Assert
+			Assert.AreEqual("♥x=1*(5+5)", output);
+		}
+	}
+}
