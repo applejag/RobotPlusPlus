c290c093c011c26b8ee9e9049be090a2bc1d489f
kalle (jag) <kalle.jillheden@me.com>
2018-04-02 13:31:23 +0200

    Updated CLI sample to use some commands

--- RobotPlusPlus.CLI/Properties/AssemblyInfo.cs
+++ RobotPlusPlus.CLI/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.18090.1668")]
-[assembly: AssemblyFileVersion("0.1.18090.1668")]
+[assembly: AssemblyVersion("0.1.18092.1378")]
+[assembly: AssemblyFileVersion("0.1.18092.1378")]
 
 [assembly: AssemblyTitle("Robot++ CLI")]
 [assembly: AssemblyDescription("Command-line interface for the Robot++ language compiler.")]
